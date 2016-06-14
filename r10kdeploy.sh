5338c251f2724cf7a6218d5766358234157f4f52
ggatward <geoff@gatwards.org>
2016-06-14 19:58:23 +1000

    i

--- r10kdeploy.sh
+++ r10kdeploy.sh
@@ -12,18 +12,40 @@ if [[ -z ${PUSH_USER} ]] || [[ -z ${SATELLITE} ]] || [[ -z ${RSA_ID} ]]; then
   exit ${WORKSPACE_ERR}
 fi
  
+# Pull out the puppet basedir from the r10k.yaml
+BASEDIR=$(cat ${WORKSPACE}/scripts/r10k.yaml | grep basedir | awk '{ print $2 }')
+
 # Push the r10k.yaml config from our GIT repo over to Satellite
-rsync --delete -va -e "ssh -l ${PUSH_USER} -i /var/lib/jenkins/.ssh/id_rsa" -va \
+rsync --delete -va -e "ssh -l ${PUSH_USER} -i ${RSA_ID}" -va \
     ${WORKSPACE}/scripts/r10k.yaml ${SATELLITE}:
 
 # use hammer on the satellite to find all capsules. Then use R10K to push the modules
-# into the puppet environmnet on each capsule
-for I in $(ssh -l ${PUSH_USER} -i ${RSA_ID} ${SATELLITE} \
-      "hammer --csv capsule list" | tail -n +2 | awk -F, '{print $2}'); do
-  ssh -l ${PUSH_USER} -i ${RSA_ID} ${I} \
-      "cd /etc/puppet ; r10k deploy environment ${R10K_ENV} -c /var/lib/jenkins/r10k.yaml -pv"
-done
+# into the puppet environmnet directory on each capsule
+ssh -l ${PUSH_USER} -i ${RSA_ID} ${SATELLITE} \
+    "cd /etc/puppet ; r10k deploy environment ${R10K_ENV} -c /var/lib/jenkins/r10k.yaml -pv"
 
 # Need to fix perms post-deploy:
-#chown -R apache: /etc/puppet/r10k/environments
-#restorecon -Fr /etc/puppet/r10k/environments
+#chown -R apache: ${BASEDIR}
+#restorecon -Fr ${BASEDIR}
+
+# Clone the updated directory to all capsules
+for I in $(ssh -l ${PUSH_USER} -i ${RSA_ID} ${SATELLITE} \
+    "hammer --csv capsule list" | tail -n +2 | awk -F, '{print $2}'); do
+echo "*** DEBUG ***"
+echo "rsync --delete -va -e \"ssh -l ${PUSH_USER} -i ${RSA_ID}\" -va ${BASEDIR}/${R10K_ENV} ${I}:${BASEDIR}"
+#  rsync --delete -va -e "ssh -l ${PUSH_USER} -i ${RSA_ID}" -va ${BASEDIR}/${R10K_ENV} ${I}:${BASEDIR}
+done
+
+
+
+# See if the environent already exits:
+if [ $(ssh -l ${PUSH_USER} -i ${RSA_ID} ${SATELLITE} \
+    "hammer environment list | awk ' /^[0-9]/ { print $3 }' | grep ${R10K_ENV}") -eq 0 ]; then
+  ssh -l ${PUSH_USER} -i ${RSA_ID} ${SATELLITE} \
+    "hammer environment create --name ${R10K_ENV} --locations 'Default Location' --organizations ${ORG}"
+fi
+
+# Import the puppet classes
+ssh -l ${PUSH_USER} -i ${RSA_ID} ${SATELLITE} \
+  "hammer proxy import-classes --id 1 --environment ${R10K_ENV}"
+

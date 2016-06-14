c8f05b6cce9130a64542a93d5cd022a82161cda8
ggatward <geoff@gatwards.org>
2016-06-14 19:13:49 +1000

    i

--- r10kdeploy.sh
+++ r10kdeploy.sh
@@ -7,7 +7,7 @@
  
 . $(dirname "${0}")/common.sh
  
-if [[ -z ${PUSH_USER} ]] || [[ -z ${SATELLITE} ]] || [[ -z ${R10K_USER} ]]; then
+if [[ -z ${PUSH_USER} ]] || [[ -z ${SATELLITE} ]] || [[ -z ${RSA_ID} ]]; then
   err "PUSH_USER, R10K_USER or SATELLITE not set or not found"
   exit ${WORKSPACE_ERR}
 fi
@@ -20,6 +20,10 @@ rsync --delete -va -e "ssh -l ${PUSH_USER} -i /var/lib/jenkins/.ssh/id_rsa" -va 
 # into the puppet environmnet on each capsule
 for I in $(ssh -l ${PUSH_USER} -i ${RSA_ID} ${SATELLITE} \
       "hammer --csv capsule list" | tail -n +2 | awk -F, '{print $2}'); do
-  ssh -l ${R10K_USER} -i ${RSA_ID} ${I} \
+  ssh -l ${PUSH_USER} -i ${RSA_ID} ${I} \
       "cd /etc/puppet ; r10k deploy environment ${R10K_ENV} -c /var/lib/jenkins/r10k.yaml -pv"
 done
+
+# Need to fix perms post-deploy:
+#chown -R apache: /etc/puppet/r10k/environments
+#restorecon -Fr /etc/puppet/r10k/environments

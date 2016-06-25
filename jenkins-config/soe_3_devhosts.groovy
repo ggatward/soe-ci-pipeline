528afd75f483209bdca137a22d0f2333c00e6266
ggatward <geoff@gatwards.org>
2016-06-25 12:51:10 +1000

    Add tests dsl

--- jenkins-config/soe_3_devhosts.groovy
+++ jenkins-config/soe_3_devhosts.groovy
@@ -4,17 +4,20 @@
  *******************************************************************/
 def hostMap = [
   'Net1 RHEL7':'buildbot1.lab.home.gatwards.org', 
-  'Net2 RHEL6':'buildbot2.lab.home.gatwards.org',
+  'Net1 RHEL6':'buildbot2.lab.home.gatwards.org',
 ]
 
 
 /*******************************************************************
- * Create the host-build jobs for each host
+ * Loop through each host...
  *******************************************************************/
 
 for (desc in hostMap.keySet()) {
   def host = hostMap.get(desc)
 
+  /*******************************************************************
+   * Create the Build-Watch jobs for each host
+   *******************************************************************/
   freeStyleJob("SOE1/Development/Build_${desc}") {
     description("Monitor the build status of ${desc} - ${host}")
     displayName("05: Build ${desc} host")
@@ -50,20 +53,70 @@ for (desc in hostMap.keySet()) {
     steps {
       shell("""
 echo "#####################################################"
-echo "#            PUSHING TESTS to TEST VMS              #"
+echo "#          WAITING FOR BUILD COMPLETION             #"
 echo "#####################################################"
 /bin/bash -x \${WORKSPACE}/scripts/waitforbuild.sh ${host}
       """)
     }
     publishers {
       downstream("Test_${desc}", 'SUCCESS')
+    }
+  }
+
+
+  /*******************************************************************
+   * Create the Test jobs for each host
+   *******************************************************************/
+  freeStyleJob("SOE1/Development/Test_${desc}") {
+    description("Run functional tests on ${desc} - ${host}")
+    displayName("06: Test ${desc} host")
+    blockOnDownstreamProjects()
+    blockOnUpstreamProjects()
+    properties {
+      buildDiscarder {
+        strategy {
+          logRotator {
+            numToKeepStr('5')
+            artifactDaysToKeepStr('')
+            artifactNumToKeepStr('')
+            daysToKeepStr('')
+          }
+        }
+      }
+    }
+    scm {
+      cloneWorkspaceSCM {
+        parentJobName('SOE1/Development/GIT_Checkout')
+        criteria('')
+      }
+    }
+    wrappers {
+      preBuildCleanup()
+      environmentVariables {
+        propertiesFile('scripts/PARAMETERS')
+      }
+    }
+    steps {
+      shell("""
+echo "#####################################################"
+echo "#            PUSHING TESTS to TEST VMS              #"
+echo "#####################################################"
+/bin/bash -x \${WORKSPACE}/scripts/pushtests.sh ${host}
+      """)
+    }
+    publishers {
+      tapPublisher {
+        failIfNoResults(true)
+        failedTestsMarkBuildAsFailure(true)
+        planRequired(true)
+        validateNumberOfTests(true)
+        outputTapToConsole(true)
+        enableSubtests(true)
+        testResults('test_results/*.tap')
+      }
       mailer('$EMAIL_TO', true, false)
     }
   }
 
 
-
-
 } /* End for loop */
-
-

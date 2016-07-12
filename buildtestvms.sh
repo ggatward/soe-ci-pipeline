de390c60d03582361a8a450547e24d593f262e4a
ggatward <geoff@gatwards.org>
2016-07-12 15:57:03 +1000

    i

--- buildtestvms.sh
+++ buildtestvms.sh
@@ -30,7 +30,7 @@ fi
 #get_test_vm_list # populate TEST_VM_LIST
 
 function get_dev_vm_list() {
-  hostlist=$(grep -A20 'def devHosts' ${WORKSPACE}/jenkins-config/soe_2_dev.groovy \
+  hostlist=$(grep -A20 'def devHosts' ${WORKSPACE}/scripts/jenkins-config/soe_2_dev.groovy \
     | grep -B20 "]" | grep : | awk -F: '{ print $2 }' | tr -d "\',")
   TEST_VM_LIST=( $hostlist )
 }

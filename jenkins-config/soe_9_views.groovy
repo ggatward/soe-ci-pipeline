a942f55be5b6f1f4401dc62cdad46b4a2659a3d0
ggatward <geoff@gatwards.org>
2016-09-14 08:34:23 +1000

    Updated DSL

--- jenkins-config/soe_9_views.groovy
+++ jenkins-config/soe_9_views.groovy
@@ -17,6 +17,23 @@ buildPipelineView('SOE/Dev Build Pipeline') {
     }
 }
 
+buildPipelineView('SOE/Prod Build Pipeline') {
+  title 'Production SOE build pipeline'
+  filterBuildQueue()
+  displayedBuilds(1)
+  alwaysAllowManualTrigger()
+  triggerOnlyLatestJob(true)
+  consoleOutputLinkStyle(OutputStyle.NewWindow)
+  showPipelineDefinitionHeader(false)
+    configure {
+        it / 'gridBuilder'(class: 'au.com.centrumsystems.hudson.plugin.buildpipeline.DownstreamProjectGridBuilder') {
+            firstJob('SOE/Production/Promote')
+            firstJobLink('job/SOE/view/Prod%20Build%20Pipeline/job/Production/Promote/')
+        }
+    }
+}
+
+
 buildPipelineView('SOE/Development/Build Pipeline') {
   title 'Dev SOE build pipeline'
   filterBuildQueue()
@@ -30,6 +47,22 @@ buildPipelineView('SOE/Development/Build Pipeline') {
             firstJob('SOE/SOE_Checkout')
             firstJobLink('job/SOE/view/Dev%20Build%20Pipeline/job/SOE_Checkout/')
         }    
+    }
+}
+
+buildPipelineView('SOE/Production/Build Pipeline') {
+  title 'Production SOE build pipeline'
+  filterBuildQueue()
+  displayedBuilds(3)
+  alwaysAllowManualTrigger()
+  triggerOnlyLatestJob(true)
+  consoleOutputLinkStyle(OutputStyle.NewWindow)
+  showPipelineDefinitionHeader(false)
+    configure {
+        it / 'gridBuilder'(class: 'au.com.centrumsystems.hudson.plugin.buildpipeline.DownstreamProjectGridBuilder') {
+            firstJob('SOE/Production/Promote')
+            firstJobLink('job/SOE/view/Prod%20Build%20Pipeline/job/Production/Promote/')
+        }
     }
 }
 
